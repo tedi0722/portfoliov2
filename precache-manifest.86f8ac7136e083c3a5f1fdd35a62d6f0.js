f8524165980a1c4bff4a4bc177d3f25b49e911ff
tedi <tedi0722@users.noreply.github.com>
2019-01-31 17:21:46 -0800

    Updates

---
+++ precache-manifest.86f8ac7136e083c3a5f1fdd35a62d6f0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "debea872abceeee94775d98ffdb6bcc5",
+    "url": "/portfoliov2/static/media/VerigateLogo.debea872.jpg"
+  },
+  {
+    "revision": "cfb1eec58160febedace",
+    "url": "/portfoliov2/static/css/main.6bc994e8.chunk.css"
+  },
+  {
+    "revision": "f5bbc2abc773e0ae0d91",
+    "url": "/portfoliov2/static/js/1.f5bbc2ab.chunk.js"
+  },
+  {
+    "revision": "98256e333b889b9119c0",
+    "url": "/portfoliov2/static/js/runtime~main.98256e33.js"
+  },
+  {
+    "revision": "ea56e0af97a35c693804dbda6951c0ef",
+    "url": "/portfoliov2/static/media/GRNLogo.ea56e0af.jpg"
+  },
+  {
+    "revision": "cfb1eec58160febedace",
+    "url": "/portfoliov2/static/js/main.cfb1eec5.chunk.js"
+  },
+  {
+    "revision": "2f597b52f162e677b56bda7a5704d9d4",
+    "url": "/portfoliov2/static/media/ProfilePic.2f597b52.JPG"
+  },
+  {
+    "revision": "7fa9a751159c8c031eafbc0ff8564d91",
+    "url": "/portfoliov2/static/media/GRNHome.7fa9a751.jpg"
+  },
+  {
+    "revision": "83e375fe95e02ae443a5a8ac9099d39a",
+    "url": "/portfoliov2/static/media/GRNOrder.83e375fe.jpg"
+  },
+  {
+    "revision": "d6e64b3f155031be7f9a1834b47ad7fe",
+    "url": "/portfoliov2/static/media/GRNWeatherNews.d6e64b3f.jpg"
+  },
+  {
+    "revision": "90b243f4396d06b7855bcef18ce1aca3",
+    "url": "/portfoliov2/index.html"
+  }
+];
