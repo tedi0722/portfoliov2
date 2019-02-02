3e3468c7723b51a0c8d0df96fbe455e475e05ba5
tedi <tedi0722@users.noreply.github.com>
2019-02-02 10:22:23 -0800

    hover gif

--- src/pages/Work/Work.jsx
+++ src/pages/Work/Work.jsx
@@ -6,15 +6,17 @@ import ProjectCard from '../../components/ProjectCard/ProjectCard';
 import GRNLogo from '../../assets/GRNLogo.jpg';
 import VerigateLogo from '../../assets/VerigateLogo.jpg';
 import patrick from '../../assets/patrick1.gif';
+import GRNGif from '../../assets/GRNGif.gif';
+import VerigateGif from '../../assets/VerigateGif.gif';
 
 const Work = () => 
     <Grid style={styles.wrapper} >
         <Row>
             <Col xs={12} md={4}>
-                <ProjectCard path="/GingerRedNaturals" logo={GRNLogo} gif={patrick} projectName="GingerRed Naturals" projectTechnology="WordPress" />
+                <ProjectCard path="/GingerRedNaturals" logo={GRNLogo} gif={GRNGif} projectName="GingerRed Naturals" projectTechnology="WordPress" />
             </Col>
             <Col xs={12} md={4}>
-                <ProjectCard path="/Verigate" logo={VerigateLogo} gif={patrick} projectName="Verigate" projectTechnology="Full Stack Application / Express" />
+                <ProjectCard path="/Verigate" logo={VerigateLogo} gif={VerigateGif} projectName="Verigate" projectTechnology="Full Stack Application / Express" />
             </Col>
             <Col xs={12} md={4}>
                 <ProjectCard path="/BattleTactics" logo={patrick} gif={patrick} projectName="Battle Tactics" projectTechnology="React Native" />
