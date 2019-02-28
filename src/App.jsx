d5008916f3dca554636056a2cc5c723e8601a025
tedi <tedi0722@users.noreply.github.com>
2019-02-28 03:59:00 -0800

    hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import QueueAnim from 'rc-queue-anim';
 // pages
 import Work from './pages/Work/Work';
